bd44d22af5757d0a03b12d628e85b1274ee660ea
Hans Strandberg <hstrand3@volvocars.com>
2023-01-20 15:10:43 +0100

    changed stuff

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -21,7 +21,7 @@ func Test_handler(t *testing.T) {
 	}{
 		{
 			"status OK",
-			args{"/", http.StatusOK, fmt.Sprintf("Helllo Americans from %s", host)},
+			args{"/", http.StatusOK, fmt.Sprintf("Hello Sweden from %s", host)},
 		},
 	}
 
