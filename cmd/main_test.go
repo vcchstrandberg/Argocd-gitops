660a4159d8b7c411c8070ab391f13fe89bfccc4d
Hans Strandberg <hstrand3@volvocars.com>
2023-01-20 19:34:03 +0100

    Changed to HelloDevOps

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -21,7 +21,7 @@ func Test_handler(t *testing.T) {
 	}{
 		{
 			"status OK",
-			args{"/", http.StatusOK, fmt.Sprintf("Hello SwedenTest123 from %s", host)},
+			args{"/", http.StatusOK, fmt.Sprintf("Hello DevOps from %s", host)},
 		},
 	}
 
