d4a162196a388776b8d5e48a24285555e3884431
kannapppan senthilnathan <kannappan.senthilnathan@volvocars.com>
2023-01-20 15:19:55 +0100

    trigger ci cd

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -21,7 +21,7 @@ func Test_handler(t *testing.T) {
 	}{
 		{
 			"status OK",
-			args{"/", http.StatusOK, fmt.Sprintf("Hello SwedenTest from %s", host)},
+			args{"/", http.StatusOK, fmt.Sprintf("Hello SwedenTest2 from %s", host)},
 		},
 	}
 
